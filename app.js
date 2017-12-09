db120b1bad20a4071c76049c557f961efbc5921f
Greg Church <greggypc@users.noreply.github.com>
2017-12-09 16:58:41 -0600

    layout html

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+// Initialize Firebase
+   var config = {
+    apiKey: "AIzaSyBP-rFcXQ1n50pK97s4SL85tSSMmncAy8Q",
+    authDomain: "fir-testproject-61539.firebaseapp.com",
+    databaseURL: "https://fir-testproject-61539.firebaseio.com",
+    projectId: "fir-testproject-61539",
+    storageBucket: "",
+    messagingSenderId: "169699289852"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
