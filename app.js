94fdf1c4f844690c7e234222f8eef051396daaec
Greg Church <greggypc@users.noreply.github.com>
2017-12-09 19:36:02 -0600

    mostly complete and working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
     authDomain: "fir-testproject-61539.firebaseapp.com",
     databaseURL: "https://fir-testproject-61539.firebaseio.com",
     projectId: "fir-testproject-61539",
-    storageBucket: "",
+    storageBucket: "fir-testproject-61539.appspot.com",
     messagingSenderId: "169699289852"
   };
   firebase.initializeApp(config);
@@ -22,16 +22,17 @@ $("#buttonAddTrain").on("click", function(event) {
   var trainTime = moment($("#trainTimeInput").val().trim(), 'HHmm').format("X");
   var frequency = $("#frequencyInput").val().trim();
 
-  // Creates local "temporary" object for holding employee data
-  var newTrain = {
-    name: trainName,
-    destination: destination,
-    time: trainTime,
-    frequency: frequency
-  };
-
-  // Uploads employee data to the database
-  database.ref().push(newTrain);
+  //Check for all inputs
+	if(trainName === '' || destination === '' || trainTime === '' || frequency === '') {
+	  alert("Please fill out all fields.");
+	}else {
+      database.ref().push({
+  	  name: trainName,
+      destination: destination,
+      time: trainTime,
+      frequency: frequency
+  });
+  }
 
   // Clears all of the text-boxes
   $("#trainNameInput").val("");
@@ -43,29 +44,19 @@ $("#buttonAddTrain").on("click", function(event) {
 
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-  console.log(childSnapshot.val());
-
   // Store everything into a variable.
   var trainName = childSnapshot.val().name;
   var destination = childSnapshot.val().destination;
   var trainTime = childSnapshot.val().time;
   var frequency = childSnapshot.val().frequency;
 
-  // Prettify the employee start
-  var trainTimeFormatted = moment.unix(trainTime).format('HHmm');
-
-  var nextArrival = 12345;
-
-  var minsAway = 54321;
-
-  // Calculate the months worked using hardcore math
-  // To calculate the months worked
-  // var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
-  // console.log(empMonths);
-
-  // Calculate the total billed rate
-  // var empBilled = empMonths * empRate;
-  // console.log(empBilled);
+  //calculations
+  var firstTrainTime = moment(trainTime, "HH:mm").subtract(1,"years");
+  var currentTime = moment();
+  var diffTime = moment().diff(moment(firstTrainTime), "minutes");
+  var timeRemainder = diffTime % frequency;
+  var minsAway = frequency - timeRemainder;
+  var nextArrival = moment().add(minsAway, "minutes").format("HH:mm A");
 
   // Add each train's data into the table
   $("#trainSchedule> tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
